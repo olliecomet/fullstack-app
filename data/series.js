56220cd50fa733a6b1df8c1c7f3b6922cde6af89
ollie <leyechi@gmail.com>
2019-08-26 13:36:58 -0700

    data

---
+++ data/series.js
@@ -0,0 +1,191 @@
+module.exports = [
+    {   
+        title: 'Tower of God',
+        author: 'SIU',
+        chapters: 441,
+        ongoing: true,
+        genre: 'Fantasy',
+        medium: 'Manhua',
+        url: 'placeholder'
+    },
+    {   
+        title: 'unOrdinary',
+        author: 'uru-chan',
+        chapters: 154,
+        ongoing: true,
+        genre: 'Fanatasy',
+        medium: 'Webcomic',
+        url: 'placeholder'
+    },
+    {   
+        title: 'The Devil is a Handsome Man',
+        author: 'Hellothisisangle',
+        chapters: 71,
+        ongoing: true,
+        genre: 'romance',
+        medium: 'webcomic',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Bomic: A Bom Comic',
+        author: 'c2oh',
+        chapters: 78,
+        ongoing: true,
+        genre: 'Romance',
+        medium: 'Webcomic',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Project Shade',
+        author: 'Monotone Ink',
+        chapters: 82,
+        ongoing: true,
+        genre: 'Action',
+        medium: 'webcomic',
+        url: 'placeholder',
+    },
+    {   
+        title: 'Ava\'s Demon',
+        author: 'Michelle Czajkowski',
+        chapters: 27,
+        ongoing: true,
+        genre: 'Fantasy',
+        medium: 'Webcomic',
+        url: 'placeholder',
+    },
+    {   
+        title: 'Distant Sky',
+        author: 'Inwan Youn & Sunhee Kim',
+        chapters: 53,
+        ongoing: false,
+        genre: 'Thriller',
+        medium: 'Manhua',
+        url: 'placeholder'
+    },
+    {   
+        title: 'The Girl From the Other Side: Siúil, a Rún',
+        author: 'Nagabe',
+        chapters: 36,
+        ongoing: true,
+        genre: 'Fantasy',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Letter Bee',
+        author: 'Hiroyuki Asada',
+        chapters: 99,
+        ongoing: false,
+        genre: 'Adventure',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Claymore',
+        author: 'Norihiro Yagi',
+        chapters: 155,
+        ongoing: false,
+        genre: 'Adventure',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Dungeon Meshi',
+        author: 'Ryoko Kui',
+        chapters: 57,
+        ongoing: true,
+        genre: 'Comedy',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Noragami: Stray God',
+        author: 'Adachitoka',
+        chapters: 83,
+        ongoing: true,
+        genre: 'Action',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Saturn Apartments',
+        author: 'Hisae Iwaoka',
+        chapters: 64,
+        ongoing: false,
+        genre: 'Science Fiction',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Houseki no Kuni',
+        author: 'Haruko Ichikawa',
+        chapters: 80,
+        ongoing: true,
+        genre: 'Fantasy',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Natsume\'s Book of Friends',
+        author: 'Yuki Midorikawa',
+        chapters: 93,
+        ongoing: true,
+        genre: 'Supernatural',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'X/1999',
+        author: 'CLAMP',
+        chapters: 158,
+        ongoing: false,
+        genre: 'Thriller',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'The Morose Mononokean',
+        author: 'Kiri Wazawa',
+        chapters: 71,
+        ongoing: true,
+        genre: 'Supernatural',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Bleach',
+        author: 'Tite Kubo',
+        chapters: 686,
+        ongoing: false,
+        genre: 'Action',
+        medium: 'Manga',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Tanpopo',
+        author: 'Camilla d\'Errico ',
+        chapters: ~10,
+        ongoing: true,
+        genre: 'Fantasy',
+        medium: 'Graphic Novel',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Kabuki: The Alchemy',
+        author: 'David Mack',
+        chapters: ~5,
+        ongoing: false,
+        genre: 'Action',
+        medium: 'Graphic Novel',
+        url: 'placeholder'
+    },
+    {   
+        title: 'Wet Moon',
+        author: 'Sophie Campbell',
+        chapters: ~20,
+        ongoing: false,
+        genre: 'Slice of Life',
+        medium: 'Graphic Novel',
+        url: 'placeholder'
+    }
+];
